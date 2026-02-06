d970ca6f51620cd0748253110089493bcc510feb
Nino Keldishvili <Ninokeldishvili@gmail.com>
2026-02-06 21:00:41 +0400

    Runtime and app configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
   modules: ["@nuxt/ui", "@nuxt/eslint"],
+  runtimeConfig: {
+    openaiApiKey: "hello",
+  },
 });
